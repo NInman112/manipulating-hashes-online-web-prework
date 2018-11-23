21cb69a1db447519ef0fd3d1ea42e06bc58ce6dd
Nick Inman <nick@theoatmeal.com>
2018-11-23 19:34:01 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
       data.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
           value.each do |flavor|
-          
+          binding.pry
           if flavor == "strawberry"
            
           end
