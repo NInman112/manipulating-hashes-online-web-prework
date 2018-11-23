bbb7bf36079248850c220dca390b0b69f62ea365
Nick Inman <nick@theoatmeal.com>
2018-11-23 19:50:47 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,12 +17,14 @@ def first_challenge
   #your code here
 
   contacts.each do |person, data|
+    if person == "Freddy Mercury"
       data.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
           value.each do |flavor|
             value.delete_if == "strawberry"
         #binding.pry
           end
+        end
       end
     end
   end
